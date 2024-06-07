1a76cf98d755600b7218ab58f78b6c7b98e14e5f
byrimahesh <byrimahesh93@gmail.com>
2024-06-07 15:28:53 +0530

    mahesh:on 07

---
+++ Servlet_jsp/src/main/java/Jsp/ResultBean.java
@@ -0,0 +1,20 @@
+package Jsp;
+
+public class ResultBean {
+	private String ht;
+
+	public ResultBean(int s)
+	{
+		
+	}
+
+	public String getHt() {
+		return ht;
+	}
+
+	public void setHt(String ht) {
+		this.ht = ht;
+	}
+	
+
+}
---
+++ Servlet_jsp/src/main/java/Jsp/ResultDAO.java
@@ -0,0 +1,10 @@
+package Jsp;
+
+public class ResultDAO {
+	protected static void init(String s1)
+	{
+		//DATA BASE CONNECTION
+	}
+	
+
+}
